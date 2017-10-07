d205a439c0815b41b6dd8fc89e85469158809060
Nathan Broadbent <git@ndbroadbent.com>
2017-10-08 00:31:13 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "form-api"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 # To install the library, run the following
 #
 # python setup.py install
